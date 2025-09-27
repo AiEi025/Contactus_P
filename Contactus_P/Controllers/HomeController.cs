cf6d5aedaaaf21e490bc2b545f16a2148d7bcc1a
a-ehsani <it@sib-co.ir>
2025-09-27 13:55:31 +0330

    Add project files.

---
+++ Contactus_P/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Contactus_P.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Contactus_P.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private static LMessage message = new LMessage();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(MessageModel messages)
+        {
+            message.listmessagemodel.Add(messages);
+            return Redirect("/home/Message");
+        }
+        public IActionResult Message()
+        {
+            
+            return View(message);
+        }
+    }
+}
---
+++ Contactus_P/Models/MessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Contactus_P.Models
+{
+    public class MessageModel
+    {
+        public string FullName { get; set; }
+        public string Subject { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MessageBody { get; set; }
+        
+    }
+    public class LMessage
+    {
+        public List<MessageModel> listmessagemodel { get; set; } = new List<MessageModel>();
+    }
+}
